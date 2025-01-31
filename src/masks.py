2c1c7475103b5a38b24f394aa7d05f0abdfdebb5
Maslykou Vitalii <9319899520@mail.ru>
2025-01-31 12:56:52 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(number_cart: str) -> str:
+    """Функция для кодирования номера карты"""
+
+    return f"{number_cart[:5]} {number_cart[5:7]}** **** {number_cart[12:16]}"
+
+
+print(get_mask_card_number("1234567890999999"))
+
+
+def get_mask_account(number_score: str) -> str:
+    """Функция для кодирования счета карты"""
+
+    return f"**{number_score[12:16]}"
+
+
+print(get_mask_account("0987654321000000"))
