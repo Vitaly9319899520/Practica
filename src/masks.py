15a38e02f17f2f973f88c5bc3e6c1e3a1c0ae676
Maslykou Vitalii <9319899520@mail.ru>
2025-02-01 07:21:42 +0300

    add __main__ in maks

--- src/masks.py
+++ src/masks.py
@@ -3,8 +3,9 @@ def get_mask_card_number(number_cart: str) -> str:
 
     return f"{number_cart[:5]} {number_cart[5:7]}** **** {number_cart[12:16]}"
 
+if __name__ == "__main__":
 
-print(get_mask_card_number("1234567890999999"))
+    print(get_mask_card_number("1234567890999999"))
 
 
 def get_mask_account(number_score: str) -> str:
@@ -12,5 +13,6 @@ def get_mask_account(number_score: str) -> str:
 
     return f"**{number_score[12:16]}"
 
+if __name__ == "__main__":
 
-print(get_mask_account("0987654321000000"))
+    print(get_mask_account("0987654321000000"))
