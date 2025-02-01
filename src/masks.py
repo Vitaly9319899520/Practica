3ab383ca49605add9697736143104484e9ecfcf2
Maslykou Vitalii <9319899520@mail.ru>
2025-02-01 08:32:17 +0300

    format standart

--- src/masks.py
+++ src/masks.py
@@ -3,6 +3,7 @@ def get_mask_card_number(number_cart: str) -> str:
 
     return f"{number_cart[:5]} {number_cart[5:7]}** **** {number_cart[12:16]}"
 
+
 if __name__ == "__main__":
 
     print(get_mask_card_number("1234567890999999"))
@@ -12,6 +13,7 @@ def get_mask_account(number_score: str) -> str:
     """Функция для кодирования счета карты"""
 
     return f"**{number_score[12:16]}"
+
 
 if __name__ == "__main__":
 
--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,33 @@
-import masks
-from typing import Union
-import datetime
 from datetime import datetime
 
+import masks
 
-def mask_account_card(tip_number_cart:Union[str,int]) -> str:
-    digit_card = ''
-    alpha_card = ''
+
+def mask_account_card(tip_number_cart: str) -> str:
+    """Функция маски"""
+
+    digit_card = ""
+    alpha_card = ""
     list_number = tip_number_cart.split()
 
-
-    if ' '.join(list_number[:2]) == 'Visa Platinum' or 'Maestro' in list_number \
-            or ' '.join(list_number[:2]) == 'Visa Classic' \
-            or ' '.join(list_number[:2]) == 'Visa Gold' or 'MasterCard' in list_number :
+    if (
+        " ".join(list_number[:2]) == "Visa Platinum"
+        or "Maestro" in list_number
+        or " ".join(list_number[:2]) == "Visa Classic"
+        or " ".join(list_number[:2]) == "Visa Gold"
+        or "MasterCard" in list_number
+    ):
 
         for i in list_number:
             if i.isalpha():
-                alpha_card += i + ' '
-                #alpha_card.append(i)
+                alpha_card += i + " "
+                # alpha_card.append(i)
 
             elif i.isdigit():
                 digit_card += i
             shifr_card = masks.get_mask_card_number(digit_card)
 
-    elif 'Счет' in list_number:
+    elif "Счет" in list_number:
         for i in list_number:
             if i.isalpha():
                 alpha_card += i
@@ -31,14 +35,17 @@ def mask_account_card(tip_number_cart:Union[str,int]) -> str:
                 digit_card += i
             shifr_card = masks.get_mask_account(digit_card)
 
+    return f"{alpha_card} {shifr_card}"
 
-    return f'{alpha_card} {shifr_card}'
 
-print(mask_account_card('MasterCard 7000792289606361'))
+print(mask_account_card("MasterCard 7000792289606361"))
 
 
 def get_date(my_date: str) -> str:
+    """Функция перевода формата даты"""
+
     date_obj = datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
     return date_obj.strftime("%d.%m.%Y")
 
-print(get_date('2024-03-11T02:26:18.671407'))
+
+print(get_date("2024-03-11T02:26:18.671407"))
