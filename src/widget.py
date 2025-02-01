154917e7a189e5fd1fea0000cf0204c2f374afa4
Maslykou Vitalii <9319899520@mail.ru>
2025-02-01 08:04:42 +0300

    update widget

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,19 @@
 import masks
 from typing import Union
+import datetime
+from datetime import datetime
 
 
 def mask_account_card(tip_number_cart:Union[str,int]) -> str:
     digit_card = ''
     alpha_card = ''
     list_number = tip_number_cart.split()
-    #print(list_number)
 
-    #if 'Maestro' in list_number or 'Visa' in list_number:
-    if ' '.join(list_number[:2]) == 'Visa Platinum' or 'Maestro' in list_number:
+
+    if ' '.join(list_number[:2]) == 'Visa Platinum' or 'Maestro' in list_number \
+            or ' '.join(list_number[:2]) == 'Visa Classic' \
+            or ' '.join(list_number[:2]) == 'Visa Gold' or 'MasterCard' in list_number :
+
         for i in list_number:
             if i.isalpha():
                 alpha_card += i + ' '
@@ -30,4 +34,11 @@ def mask_account_card(tip_number_cart:Union[str,int]) -> str:
 
     return f'{alpha_card} {shifr_card}'
 
-print(mask_account_card('Visa Platinum 7000792289606361'))
+print(mask_account_card('MasterCard 7000792289606361'))
+
+
+def get_date(my_date: str) -> str:
+    date_obj = datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
+
+print(get_date('2024-03-11T02:26:18.671407'))
